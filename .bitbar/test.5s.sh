368e479f646292ddf184e3b5d3d83a2850957aaa
Chris <chris@distrail.io>
2020-03-21 20:32:05 +0000

    added bitbar with test

---
+++ .bitbar/test.5s.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "testing|font=hack nerd font"
+echo "---"
+echo "test drop down"
+echo "another test"
+echo "--sub|color=red"
+echo "--top"
+echo "--menu"
+echo "kdfn"
+echo "kndfdkf"
+
