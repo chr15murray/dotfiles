65daf130cfd15b9d448fb292023d47a6bcbb8b3a
chr15murray <chris.murray@distrail.io>
2020-03-23 14:08:13 +0000

    bitbar replacing clock

---
+++ bitbar/clockreplace.5s.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+titleemoji=":monkey_face:"
+datetime=$(date "+%H:%M %a %d %b")
+
+echo "${titleemoji} ${datetime}"
+echo "---"
+echo ":calendar: ${datetime} (Open Cal) | href=https://calendar.google.com"
+
