f81fdb32eb3abf5d5b83d265c472d1dd4d544a3d
chr15murray <chris.murray@distrail.io>
2020-03-26 15:55:31 +0000

    installed typora and updated wakatime bitbar

--- bitbar/wakatime.10m.sh
+++ bitbar/wakatime.10m.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo "🕒 $(/usr/local/bin/wakatime --today) | href=https://wakatime.com/dashboard"
+wtoday=$(/usr/local/bin/wakatime --today)
+
+wtoday_browsing=$( echo $wtoday | cut -d , -f1)
+wtoday_coding=$( echo $wtoday | cut -d , -f2)
+
+echo "🕸 ${wtoday_browsing}"
+echo "#⃣ ${wtoday_coding}"
+
+echo "---"
+echo "Open Wakatime | href=https://wakatime.com/dashboard"
