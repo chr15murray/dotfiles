f35095432d1c8f6a29b6fe670920992b53192ac6
chr15murray <chris.murray@distrail.io>
2020-04-04 12:32:33 +0100

    updated mac settings

--- .config/yadm/scripts/mac-settings.sh
+++ .config/yadm/scripts/mac-settings.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
 
-# Set Mac Settings as requires
+# Set Mac Settings as desired
+
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Ask for the administrator password upfront
+sudo -v
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+#### System Preferences
+### > General > Appearance
+# Dark mode
+defaults write -globalDomain AppleInterfaceStyle -string Dark
+
+### > Dock
+defaults write com.apple.dock tilesize -int 36
+defaults write com.apple.dock orientation -string left
+
+## > Automatically hide and show the Dock:
+defaults write com.apple.dock autohide -bool true
+
+### >Mission Controll > Automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+### SIRI - 47
+
+### Trackpad > Tap to click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+
+
+
 
 # Finder
 ## Show the path bar
@@ -24,4 +56,5 @@ defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 ## Fast key repeat.
 defaults write NSGlobalDomain KeyRepeat -int 2
 
-
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
