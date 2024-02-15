b7a4aa7ca9419edfeb80cddce491425e217fad3d
Chris Murray <chris@distrail.io>
2024-02-15 18:47:02 +0000

    switch to lazyvim

---
+++ .config/nvim/lua/plugins/core.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
+
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+
+  {
+    "Mofiqul/dracula.nvim",
+    opts = {
+      transparent_bg = true,
+    },
+  },
+}
