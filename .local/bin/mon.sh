1f73391cba46f8daf4cb499846d4b5c8c42a3930
chr15murray <chris@distrail.io>
2024-08-18 16:18:13 +0100

    rebuild updates

--- .local/bin/mon.sh
+++ .local/bin/mon.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash 
 
-hyprctl monitors >/tmp/monitor
+hyprctl monitors > /tmp/monitor
 
 var=$(sed -n '1{p;q}' /tmp/monitor | awk '{ print $2 }')
 
@@ -8,10 +8,19 @@ var1=$(sed -n '2{p;q}' /tmp/monitor | awk '{ print $1 }')
 
 var2=$(sed -n '2{p;q}' /tmp/monitor | awk '{ print $3 }')
 
-#var3=$(sed -n '10{p;q}' /tmp/monitor | awk '{ print $2 }')
-# Hardcode the scaling factor
-var3=1.00
+var3=$(sed -n '10{p;q}' /tmp/monitor | awk '{ print $2 }')
 
 var4="$var, $var1, $var2, $var3"
 
+var5=${var1%%x*}
+
 sed -i "/monitor=/c\monitor= $var4" .config/hypr/hyprland.conf
+
+sed -i "/\"width\":/c\    \"width\":$var5," .config/waybar/config
+
+sed -i "s/USER/$USER/" .config/waybar/style.css
+
+sed -i "s|exec-once = .local/bin/mon.sh|#exec-once = .local/bin/mon.sh|" $HOME/.config/hypr/hyprland.conf
+
+sed -i "s|exec-once = garuda-welcome|#exec-once = garuda-welcome|" $HOME/.config/hypr/hyprland.conf
+
